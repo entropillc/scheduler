4bb80e82cc8c5697184c4a9f10598367e3da4f73
Nicholas Watson <nwwatson@gmail.com>
2011-11-09 17:01:00 -0500

    added room model and controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,65 @@
+class RoomsController < ApplicationController
+  respond_to :json
+  
+  # GET /rooms
+  # GET /rooms.json
+  def index
+    @rooms = Room.all
+
+    render json: @rooms 
+  end
+
+  # GET /rooms/1
+  # GET /rooms/1.json
+  def show
+    @room = Room.find(params[:id])
+
+    render json: @room
+  end
+
+  # GET /rooms/new
+  # GET /rooms/new.json
+  def new
+    @room = Room.new
+
+    render json: @room 
+  end
+
+  # GET /rooms/1/edit
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  # POST /rooms
+  # POST /rooms.json
+  def create
+    @room = Room.new(params[:room])
+
+    if @room.save
+      render json: @room, status: :created, location: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /rooms/1
+  # PUT /rooms/1.json
+  def update
+    @room = Room.find(params[:id])
+
+    if @room.update_attributes(params[:room])
+      head :ok
+    else
+      render json: @room.errors, status: :unprocessable_entity 
+    end
+  end
+
+  # DELETE /rooms/1
+  # DELETE /rooms/1.json
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+
+    head :ok 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Scheduler::Application.routes.draw do
   
+  resources :rooms
+
   root :to => 'welcome#index'
   
   # The priority is based upon order of creation:
